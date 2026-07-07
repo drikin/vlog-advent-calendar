977d12edb932c5b25764b16b4efd19d39affdd29
Kohichi Aoki <drikin@gmail.com>
2026-07-07 16:55:47 -0700

    title: デスブログに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BSM地獄のVLOG強化月間 2026.07",
+  title: "デスブログ - VLOG強化月間 アドベントカレンダー",
   description: "毎日更新されるVlogをアドベントカレンダー形式でチェック！",
 };
 
